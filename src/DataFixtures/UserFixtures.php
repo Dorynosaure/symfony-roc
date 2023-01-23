8b72d42eb03290136dbe42cd269eac19afb74e17
rhul0001 <axel.rhul@etudiant.univ-reims.fr>
2023-01-23 23:31:16 +0100

    Ajout du hash du password

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\User;
+use App\Factory\CategoryFactory;
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -12,6 +13,6 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         UserFactory::createMany(50);
-        UserFactory::createOne(['lastName'=>'Rhul','firstName'=>'Axel','email'=>'axel.rhul@gmail.com','roles'=>['ROLE_ADMIN','ROLE_USER'],'category'=>null]);
+        UserFactory::createOne(['lastName'=>'Rhul','firstName'=>'Axel','email'=>'axel.rhul@gmail.com','password'=>'test','roles'=>['ROLE_ADMIN','ROLE_USER']]);
     }
 }
--- src/Factory/UserFactory.php
+++ src/Factory/UserFactory.php
@@ -4,6 +4,7 @@ namespace App\Factory;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Zenstruck\Foundry\ModelFactory;
 use Zenstruck\Foundry\Proxy;
 use Zenstruck\Foundry\RepositoryProxy;
@@ -29,15 +30,15 @@ use Zenstruck\Foundry\RepositoryProxy;
  */
 final class UserFactory extends ModelFactory
 {
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
-     *
-     * @todo inject services if required
-     */
-    public function __construct()
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
         parent::__construct();
+
+        $this->passwordHasher = $passwordHasher;
     }
+
 
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
@@ -55,15 +56,15 @@ final class UserFactory extends ModelFactory
         ];
     }
 
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
-     */
     protected function initialize(): self
     {
         return $this
-            // ->afterInstantiate(function(User $user): void {})
-        ;
+            ->afterInstantiate(function (User $user) {
+                $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPassword()));
+            })
+            ;
     }
+
 
     protected static function getClass(): string
     {
